aa1555867e64ab44268087fa60af4d4d7ccb26cf
longquanxiao <1186481819@qq.com>
2019-05-27 14:13:30 +0800

    前端控制器核心编写

---
+++ src/utils/Dispatcher.java
@@ -0,0 +1,30 @@
+package utils;
+
+// 这是处理前端请求的前端控制器
+public class Dispatcher {
+
+    //TODO 处理登陆请求的
+    public static void handleLogin(){
+
+    }
+
+    //TODO 处理登出请求
+    public static void handleLogout(){
+
+    }
+
+    //TODO 处理转发数据请求的
+    public static void handleTransmit(){
+
+    }
+
+    //TODO 处理对局匹配请求
+    public static void handleMatch(){
+
+    }
+
+    //TODO 处理游戏结束的请求
+    public static void handleGameOver(){
+
+    }
+}
